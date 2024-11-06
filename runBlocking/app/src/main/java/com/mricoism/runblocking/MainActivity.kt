c277e79b2a884cadeafe8cc6144d2fedf07c2163
mricoism <mrikoismail73@gmail.com>
2024-11-06 20:57:38 +0700

    Add and setup project RunBlock

---
+++ runBlocking/app/src/main/java/com/mricoism/runblocking/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.mricoism.runblocking
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mricoism.runblocking.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val TAG = "MainActivityRIKO"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "MAILLLL"
+        binding.Button.setOnClickListener {
+            // DO SOMETING HERE
+        }
+
+        GlobalScope.launch {
+//            delay(1000L) // This is suspend function if you see documentation
+            val networkCallAnswer = doNetworkCall() // This first delay call will affect the second delay call. So it will add up, Because they are executed in the same coroutine.
+            val networkCallAnswer2 = doNetworkCall2()
+            Log.d(TAG, networkCallAnswer)
+            Log.d(TAG, networkCallAnswer2)
+        }
+    }
+    // suspend function, only can executed inside another suspend function or coroutine scope like GlobalScope.launch
+    suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2(): String {
+        delay(3000L)
+        return "This is the answer 2"
+    }
+}
