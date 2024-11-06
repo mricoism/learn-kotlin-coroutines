9bb9c0510c4b69b067eb875215ab3a410fda1d33
mricoism <mrikoismail73@gmail.com>
2024-11-06 11:03:06 +0700

    Setup project SuspendFunction

--- SuspendFunctions/app/src/main/java/com/mricoism/suspendfunctions/MainActivity.kt
+++ SuspendFunctions/app/src/main/java/com/mricoism/suspendfunctions/MainActivity.kt
@@ -1,2 +1,31 @@
 package com.mricoism.suspendfunctions
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mricoism.suspendfunctions.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "MAILLLL"
+        binding.Button.setOnClickListener {
+            // DO SOMETING HERE
+        }
+
+        GlobalScope.launch {
+            delay(1000L) // This is suspend function if you see documentation
+        }
+    }
+
+//    suspend fun d
+}
