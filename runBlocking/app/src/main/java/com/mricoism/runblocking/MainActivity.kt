0012e3165a35369680d18aef72b85c23c7e711da
mricoism <mrikoismail73@gmail.com>
2024-11-07 09:51:38 +0700

    Finish project runBlocking

--- runBlocking/app/src/main/java/com/mricoism/runblocking/MainActivity.kt
+++ runBlocking/app/src/main/java/com/mricoism/runblocking/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.mricoism.runblocking.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity: AppCompatActivity() {
 
@@ -19,27 +21,39 @@ class MainActivity: AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textView.text = "MAILLLL"
-        binding.Button.setOnClickListener {
-            // DO SOMETING HERE
+        Log.d(TAG, "Before runBlocking")
+        GlobalScope.launch(Dispatchers.Main) {
+            // This wont block the thread, and we still can use our UI.
+            delay(3000L)
         }
 
-        GlobalScope.launch {
-//            delay(1000L) // This is suspend function if you see documentation
-            val networkCallAnswer = doNetworkCall() // This first delay call will affect the second delay call. So it will add up, Because they are executed in the same coroutine.
-            val networkCallAnswer2 = doNetworkCall2()
-            Log.d(TAG, networkCallAnswer)
-            Log.d(TAG, networkCallAnswer2)
-        }
-    }
-    // suspend function, only can executed inside another suspend function or coroutine scope like GlobalScope.launch
-    suspend fun doNetworkCall(): String {
-        delay(3000L)
-        return "This is the answer"
-    }
+        // Question: WHY WE NEED THIS ?
+        runBlocking {
+            // This is coroutine scope too.
+            // this actually will block the main threads, and will block our UI update.
 
-    suspend fun doNetworkCall2(): String {
-        delay(3000L)
-        return "This is the answer 2"
+
+            // And we can start new coroutine like inside GlobalScope.launch, By writing launch inside runBlocking.
+            // this code bellow won't add up. Because its will executed at the same time (Async).
+            launch(Dispatchers.IO) {
+                delay(10_000L)
+                Log.d(TAG, "Finish IO Coroutine 1")
+            }
+            launch(Dispatchers.IO) {
+                delay(10_000L)
+                Log.d(TAG, "Finish IO Coroutine 2")
+            }
+
+            Log.d(TAG, "Start of runBlocking")
+            delay(5000L)
+            Log.d(TAG, "End of blocking")
+            Log.d(TAG, "After runBlocking")
+        }
+
+//        Thread.sleep(5000L) // This no need call inside coroutine scope
+//        delay(3000L) // This need to call inside Coroutine scope. And runBlocing actually give you way to call delay without coroutines scope. LIKE THIS
+
+
+
     }
 }
