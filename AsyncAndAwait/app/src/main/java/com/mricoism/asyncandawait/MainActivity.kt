78d7107fde9a8622ad86614887394fa1dbffcb2a
mricoism <mrikoismail73@gmail.com>
2024-11-07 15:46:45 +0700

    Setup project AsyncAndAwait

--- AsyncAndAwait/app/src/main/java/com/mricoism/asyncandawait/MainActivity.kt
+++ AsyncAndAwait/app/src/main/java/com/mricoism/asyncandawait/MainActivity.kt
@@ -1,4 +1,28 @@
 package com.mricoism.asyncandawait
 
-class MainActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mricoism.asyncandawait.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val TAG = "MainActivityRIKO"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+
 }
